44082812186bdab7f470c46871b2e2cbfe1604f3
Jonathan Anchia <jonhanchia2@gmail.com>
2022-12-09 16:31:20 -0600

    Jonathan Anchia
    Signed-off-by: Jonathan Anchia <jonhanchia2@gmail.com>

--- app/src/main/java/com/example/tarea2/adapater/ProductoAdapter.kt
+++ app/src/main/java/com/example/tarea2/adapater/ProductoAdapter.kt
@@ -4,6 +4,7 @@ import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.navigation.findNavController
 import androidx.recyclerview.widget.RecyclerView
+import com.example.tarea2.databinding.ProductosBinding
 import com.example.tarea2.model.Producto
 
 class ProductoAdapter : RecyclerView.Adapter<ProductoAdapter.ProductoViewHolder>() {
@@ -16,21 +17,24 @@ class ProductoAdapter : RecyclerView.Adapter<ProductoAdapter.ProductoViewHolder>
             notifyDataSetChanged()
         }
 
-        inner class ProductoViewHolder(private val itemBinding: ProductoFilaBinding) : RecyclerView.ViewHolder(itemBinding.root){
-            fun dibuja(producto: Producto){
+        inner class ProductoViewHolder(private val itemBinding: ProductosBinding) : RecyclerView.ViewHolder(itemBinding.root) {
+            fun dibuja(producto: Producto) {
                 itemBinding.tvNombre.text = producto.nombre
                 itemBinding.tvCorreo.text = producto.correo
                 itemBinding.tvTelefono.text = producto.telefono
-                // Evento enviar Update
-                itemBinding.vistaFila.setOnClickListener{
-                    val accion = HomeFragmentDirections.actionNavHomeToUpdateProductoFragment(producto)
-                    itemView.findNavController().navigate(accion)
-                }
+                        // Evento enviar Update
+                        itemBinding.vistaFila.setOnClickListener {
+                            val accion =
+                                HomeFragmentDirections.actionNavHomeToUpdateProductoFragment(
+                                    producto
+                                )
+                            itemView.findNavController().navigate(accion)
+                        }
 
             }
         }
 
-        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ProductoViewHolder {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ProductoViewHolder {
             val itemBinding = ProductoFilaBinding.inflate(LayoutInflater.from(parent.context),parent,false)
             return ProductoViewHolder(itemBinding)
         }
--- app/src/main/java/com/example/tarea2/model/Producto.kt
+++ app/src/main/java/com/example/tarea2/model/Producto.kt
@@ -8,14 +8,12 @@ import kotlinx.parcelize.Parcelize
 data class Producto (
     var id: String,
     val nombre: String,
-    val vendedor: String,
     val precio: String,
-    val descripcion: String,
     val telefono: String,
     val correo: String?
 
 ): Parcelable {
 
     constructor():
-                this("","","","","","","")
+                this("","","","","")
 }
